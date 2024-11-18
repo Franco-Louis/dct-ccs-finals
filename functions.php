1658d25e7cfa1c1db9ec24b767a73f16faa0832f
Franco-Louis <deverafranco1207@gmail.com>
2024-11-18 17:37:58 +0800

    Added Functions

--- functions.php
+++ functions.php
@@ -1,3 +1,35 @@
-<?php    
-    // All project functions should be placed here
+<?php
+
+function validateLoginCredentials($email, $password) {
+    $arrErrors = [];
+    
+    // Sanitize input data
+    $email = sanitizeInput($email);
+    $password = sanitizeInput($password);
+
+    // Validate email
+    if ($email === '') {
+        $arrErrors[] = 'Email is required.';
+    } elseif (!isValidEmail($email)) {
+        $arrErrors[] = 'Invalid email.';
+    }
+
+    // Validate password
+    if ($password === '') {
+        $arrErrors[] = 'Password is required.';
+    }
+
+    return $arrErrors;
+}
+
+// Helper function to sanitize input
+function sanitizeInput($input) {
+    return htmlspecialchars(stripslashes(trim($input)));
+}
+
+// Helper function to validate email
+function isValidEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+}
+
 ?>
